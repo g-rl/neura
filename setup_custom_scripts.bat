cab546ab20591017e121980e3a6df4d4e578653b
m <mjkzyalt@gmail.com>
2026-03-19 21:36:34 -0500

    fix

--- setup_custom_scripts.bat
+++ setup_custom_scripts.bat
@@ -9,9 +9,10 @@ if %errorlevel% neq 0 (
 
 setlocal
 
-:: Get the directory this script lives in (the custom_scripts folder)
+:: The custom_scripts folder sits next to this script
 set "SCRIPT_DIR=%~dp0"
 if "%SCRIPT_DIR:~-1%"=="\" set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+set "SOURCE=%SCRIPT_DIR%\custom_scripts"
 
 :: Ask for IW8 mod directory
 set /p "IW8_DIR=Enter IW8 mod directory (iw8-mod): "
@@ -21,13 +22,10 @@ set "IW8_DIR=%IW8_DIR:"=%"
 set /p "S4_DIR=Enter S4 mod directory (s4-mod): "
 set "S4_DIR=%S4_DIR:"=%"
 
-cls
-
-:: Create symlinks directly inside the provided paths
-mklink /D "%IW8_DIR%\custom_scripts" "%SCRIPT_DIR%"
-mklink /D "%S4_DIR%\custom_scripts" "%SCRIPT_DIR%"
+:: Create symlinks
+mklink /D "%IW8_DIR%\custom_scripts" "%SOURCE%"
+mklink /D "%S4_DIR%\custom_scripts" "%SOURCE%"
 
 echo.
-color 2
 echo Done!
 pause
